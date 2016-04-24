f6441969eee2ea3f7e09bc712a05599d9405819a
Hubert Léo <leohub@live.fr>
2016-04-24 23:04:24 +0200

    [*] Prepare search victim

--- admin/victim/localised.php
+++ admin/victim/localised.php
@@ -138,6 +138,31 @@
 <!-- DataTables -->
 <script src="/admin/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="/admin/plugins/datatables/dataTables.bootstrap.min.js"></script>
+
+
+
+
+<!--       1        -->
+
+
+
+<script src="/admin/plugins/jQuery/jQuery-2.2.0.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="/admin/bootstrap/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="/admin/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/admin/plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="/admin/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="/admin/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="/admin/dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/admin/dist/js/demo.js"></script>
+
+
+
 <script>
     $(function () {
         $("#example1").DataTable();
